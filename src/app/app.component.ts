362e8b74f642ea90afc176b7a267f7fbf25db92d
David Guedes <d2vid.guedes@gmail.com>
2024-11-04 23:15:52 -0300

    feat:configuração incial do chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { WebsocketService } from './websocket.service';
+import { FormsModule } from '@angular/forms';
+
+interface messageChat {
+  mensagem: string;
+  name_user: string;
+  new_user: boolean;
+  from_me: boolean;
+  user_id: string;
+  id_message: string;
+  private?: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'chat-websocket-angular';
+  name: string = '';
+  newMessage: string = '';
+  messages: messageChat[] = [];
+
+  protected websocketService = inject(WebsocketService);
+
+  ngOnInit(): void {
+    this.websocketService.onMessage().subscribe((dataMessage: any) => {
+      let dataAtual = new Date();
+      this.messages.push({
+        mensagem: dataMessage.msg,
+        name_user: dataMessage.user,
+        new_user: false,
+        from_me: dataMessage.user_id == this.websocketService.socketId() ? true : false,
+        user_id: dataMessage.user,
+        id_message: `${dataAtual.getTime}${dataMessage.user}`,
+        private: dataMessage.private ?? false
+      })
+    })
+  }
+
+  // Enviar mensagem
+  sendMessage() {
+    if(this.newMessage.trim()) {
+      this.websocketService.sendMessage({
+        message: this.newMessage,
+        user: this.name,
+        user_id: this.websocketService.socketId()
+      })
+      this.newMessage = '';
+    }
+  }
 }
---
+++ src/app/websocket.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, signal } from '@angular/core';
+import { Observable } from 'rxjs';
+import { io, Socket } from 'socket.io-client';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+  private socket!: Socket;
+  private socketIdWritable = signal('');
+  public socketId = this.socketIdWritable.asReadonly();
+  
+  constructor() {
+    this.createConnection();
+  }
+
+  createConnection() {
+    // Conectar ao servidor Socket.IO
+    console.log('url: ', environment.apiURL);
+    this.socket = io(`${environment.apiURL}`);
+
+    this.socket.on('connect', () => {
+      this.socketIdWritable.set(this.socket.id ?? '')
+    });
+  }
+
+  // Enviar mensagem para o servidor
+  sendMessage(dataMessage: any) {
+    this.socket.emit('message', dataMessage);
+  }
+
+  // Receber mensagens do servidor
+  onMessage(): Observable<string> {
+    return new Observable((observer) => {
+      this.socket.on('message', (dataMessage) => {
+        console.log('dataMessage: ', dataMessage);
+        observer.next(dataMessage);
+      })
+    })
+  }
+}
